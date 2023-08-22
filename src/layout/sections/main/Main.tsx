5c7e4820f36e3086fc45cb21a903e56e2510728b
Sergey <Zaikouski.sergey@gmail.com>
2023-08-22 12:06:44 +0300

    add flexWrapper

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
 import styled from "styled-components";
 import avatar from "../../../assets/images/my_avatar-webp.webp"
+import {FlexWrapper} from "layout/sections/main/FlexWrapper";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <div>
-                <span>Hi There,</span>
-                <span>My name is</span>
-                <h2>Siarhei Zaikouski</h2>
-                <h1>A Front-end Developer</h1>
-            </div>
-            <img src={avatar} alt="avatar"/>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There,</span>
+                    <MainName>I am Siarhei Zaikouski</MainName>
+                    <MainTitle>A Front-end Developer</MainTitle>
+                </div>
+                <img src={avatar} alt="avatar"/>
+            </FlexWrapper>
         </StyledMain>
     );
 };
-const StyledMain = styled.main`
-    img{
-      width: 350px;
-      height: 350px;
-      border-radius: 230px;
-      object-fit: cover;
-      background: lightgray 40% / cover -130px 0px ;
-    }
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #7bf3bb;
+
+  img {
+    width: 200px;
+    height: 200px;
+    border-radius: 230px;
+    object-fit: cover;
+    background: lightgray 40% / cover -130px 50px;
+  }
+`
+
+const MainTitle = styled.h1`
+
+`
+const MainName = styled.h2`
+
 `
 
