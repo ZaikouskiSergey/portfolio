10c8074dd5a30be3a0b74133d7f29403bac92b76
Sergey <Zaikouski.sergey@gmail.com>
2024-04-08 11:40:30 +0300

    feat: change styles for Header

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -2,22 +2,33 @@ import React from 'react';
 import styled from "styled-components";
 
 import {Logo} from "components/logo/Logo";
-import {Menu} from "components/menu/Menu";
 import {IconContact} from "layout/header/IconContact";
+import {Container} from "components/Container";
+import {FlexWrapper} from "components/FlexWrapper";
+import {HeaderMenu} from "layout/header/headerMenu/HeaderMenu";
 
 export const Header = () => {
     const menuItems = ["Home", "Skills", "Works", "Testimony", "Contact"]
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu items={menuItems}/>
-            <IconContact/>
+            <Container>
+                <FlexWrapper justify={'space-between'} align={'center'}>
+                    <Logo/>
+                    <HeaderMenu items={menuItems}/>
+                    <IconContact/>
+                </FlexWrapper>
+            </Container>
         </StyledHeader>
     );
 };
 const StyledHeader = styled.header`
-  background-color: #86ea9e;
-  display: flex;
-  justify-content: space-between;
+  background-color: #1F1F20;
+  padding: 20px 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+
 `
 
