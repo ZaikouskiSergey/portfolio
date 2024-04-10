58c7690ba1157472ae49f418cdfdfbfbda0115f2
Sergey <Zaikouski.sergey@gmail.com>
2024-04-10 13:37:29 +0300

    feat: add containers and change main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,26 +2,49 @@ import React from 'react';
 import styled from "styled-components";
 import avatar from "../../../assets/images/my_new-avatar-webp.webp"
 import {FlexWrapper} from "components/FlexWrapper";
+import {Container} from "components/Container";
+import {theme} from "styles/Theme";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={"center"} justify={"space-around"} >
-                <div>
-                    <MainSpan>Hi There,</MainSpan>
-                    <MainName>I am Siarhei Zaikouski</MainName>
-                    <MainTitle>A Front-end Developer</MainTitle>
-                </div>
-                <Photo src={avatar} alt="avatar"/>
+            <Container>
+                <FlexWrapper align={"center"} justify={"space-between"}>
+                    <div>
+                        <SmallText>Hi There,</SmallText>
+                        <MainName>I am <span>Siarhei Zaikouski</span></MainName>
+                        <MainTitle>A Front-end Developer.</MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={avatar} alt="avatar"/>
+                    </PhotoWrapper>
 
-            </FlexWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
 const StyledMain = styled.div`
-  background-color: #1F1F20; 
+  background-color: #1F1F20;
+  min-height: 100vh;
+  display: flex;
+`
 
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
 
+  &::before{
+    z-index: -1;
+    content: " ";
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+
+    position: absolute;
+    top: -24px;
+    left: 24px;   
+  }
 `
 const Photo = styled.img`
   width: 350px;
@@ -29,16 +52,37 @@ const Photo = styled.img`
   object-fit: cover;
 `
 const MainTitle = styled.h1`
-  color: #FFFFFF;
   font-size: 27px;
+  font-weight: 400
 
 `
 const MainName = styled.h2`
-  color: #FFFFFF;
+  font-family: "Josefin Sans", sans-serif;
   font-size: 50px;
+  letter-spacing: 0.05em;
+  font-weight: 700;
+  margin: 10px 0;
+
+  span {
+    position: relative;
+    z-index: 0;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+  }
+
+
 `
-const MainSpan = styled.span`
-  color: #FFFFFF;
+const SmallText = styled.span`
   font-size: 14px;
+  font-weight: 400;
 `
 
