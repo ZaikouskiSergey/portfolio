94b889f188740603babe9e1caf303951d1331a1a
Sergey <Zaikouski.sergey@gmail.com>
2024-04-11 15:04:59 +0300

    feat: update components Button and Skills

--- src/components/skill/Skill.tsx
+++ src/components/skill/Skill.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import {Icon} from "components/icon/Icon";
 import styled from "styled-components";
-type SkillProps={
+import {FlexWrapper} from "components/FlexWrapper";
+
+type SkillProps = {
     iconId: string
     title: string
     description: string
@@ -10,24 +12,43 @@ type SkillProps={
 export const Skill = ({title, iconId, description}: SkillProps) => {
     return (
         <StyledSkill>
-            <Icon iconId={iconId} width={'50'}/>
-            <SkillTitle>{title}</SkillTitle>
-            <SkillText>{description}</SkillText>
+            <FlexWrapper direction={'column'} align={'center'}>
+                <IconWrapper>
+                    <Icon iconId={iconId} width={'50'}/>
+                </IconWrapper>
+                <SkillTitle>{title}</SkillTitle>
+                <SkillText>{description}</SkillText>
+            </FlexWrapper>
         </StyledSkill>
     );
 };
 const StyledSkill = styled.div`
-  width: 30%;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin: 10px;
-  background-color: darkseagreen;
-
+  width: 380px;
+  padding: 62px 20px 40px;
 `
 const SkillTitle = styled.h3`
-
+  margin: 70px 0 15px;
+  text-transform: uppercase;
 `
 const SkillText = styled.p`
+  text-align: center;
+`
+const IconWrapper = styled.div`
+  position: relative;
 
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 80px;
+    height: 80px;
+    background: rgba(255,255,255, 0.1);
+    transform: rotate(45deg) translate(-50%, -50%) ;
+    
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform-origin: top left;
+    
+    
+  }
 `
--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -3,34 +3,36 @@ import styled from "styled-components";
 import {FlexWrapper} from "components/FlexWrapper";
 import {SectionTitle} from "components/SectionTitle";
 import {Skill} from "components/skill/Skill";
+import {Container} from "components/Container";
 
 export const Skills = () => {
     return (
         <StyledSkills>
             <SectionTitle>My Skills</SectionTitle>
-            <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-                <Skill iconId={"icon-html"}
-                       title={"My Skills"}
-                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
-                <Skill iconId={"icon-react"} title={"My Skills"}
-                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
-                <Skill iconId={"icon-react"} title={"My Skills"}
-                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
-                <Skill iconId={"icon-react"} title={"My Skills"}
-                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
-                <Skill iconId={"icon-react"} title={"My Skills"}
-                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
-                <Skill iconId={"icon-react"} title={"My Skills"}
-                       description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+            <Container>
+                <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+                    <Skill iconId={"icon-html"}
+                           title={"My Skills"}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill iconId={"icon-react"} title={"My Skills"}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill iconId={"icon-react"} title={"My Skills"}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill iconId={"icon-react"} title={"My Skills"}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill iconId={"icon-react"} title={"My Skills"}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
+                    <Skill iconId={"icon-react"} title={"My Skills"}
+                           description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"}/>
 
 
-            </FlexWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledSkills>
     );
 };
 const StyledSkills = styled.section`
-  background-color: #252527;
-  min-height: 100vh;
+  
 `
 
 
