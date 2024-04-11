0e06d620a627e25d516072a798317b4f24f52ec7
Sergey <Zaikouski.sergey@gmail.com>
2024-04-11 15:03:30 +0300

    feat: updated component Works

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,27 +1,29 @@
 import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "components/SectionTitle";
-import {Menu} from "components/menu/Menu";
 import {FlexWrapper} from "components/FlexWrapper";
 import {Work} from "layout/sections/works/work/Work";
 import picture from '../../../assets/images/snapedit_1692384718547.svg'
+import {Container} from "components/Container";
+import {TabMenu} from "layout/sections/works/tabMenu/TabMenu";
 
 export const Works = () => {
-    const worksItems = ["ALL", "LANDING PAGE", "REACT", "SPA"]
+    const worksItems = ["all", "LANDING Page", "REACT", "SPA"]
     return (
         <StyledWorks>
-            <SectionTitle>My Works</SectionTitle>
-            <Menu items={worksItems}/>
-            <FlexWrapper justify={"space-around"}>
-                <Work title={"Counter"} src={picture} text={"Lorem ipsum dolor sit amet"}/>
-                <Work title={"Social network"} src={""} text={"Lorem ipsum dolor sit amet"}/>
-            </FlexWrapper>
-
+            <Container>
+                <SectionTitle>My Works</SectionTitle>
+                <TabMenu items={worksItems}/>
+                <FlexWrapper justify={"space-between"} align={"flex-start"}>
+                    <Work title={"Counter"} src={picture} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
+                    <Work title={"Social network"} src={""} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolomagna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>
+                </FlexWrapper>
+            </Container>
         </StyledWorks>
     );
 };
 
 const StyledWorks = styled.section`
-  min-height: 100vh;
-  background-color: darkseagreen;
+  
+  
 `
--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from "styled-components";
+import {Link} from "components/Link";
+import {theme} from "styles/Theme";
+import {Button} from "components/Button";
 
 type WorkProps = {
     title: string
@@ -9,11 +12,16 @@ type WorkProps = {
 export const Work = ({title, text, src}: WorkProps) => {
     return (
         <StyledWork>
-            <Image src={src} alt={""}/>
-            <Title>{title}</Title>
-            <Text>{text}</Text>
-            <Link href={"#"}>Demo</Link>
-            <Link href={"#"}>Code</Link>
+            <ImageWrapper>
+                <Image src={src} alt={""}/>
+                <Button>view project</Button>
+            </ImageWrapper>
+            <Description>
+                <Title>{title}</Title>
+                <Text>{text}</Text>
+                <Link href={"#"}>Demo</Link>
+                <Link href={"#"}>Code</Link>
+            </Description>
 
         </StyledWork>
     );
@@ -21,22 +29,74 @@ export const Work = ({title, text, src}: WorkProps) => {
 const StyledWork = styled.div`
   width: 100%;
   max-width: 540px;
+  background-color: ${theme.colors.secondaryBg};
   object-fit: cover;
+
+  ${Link} {
+    padding: 10px 0;
+
+    & + ${Link} {
+      margin-left: 20px;
+    }
+  }
+`
+
+const Description = styled.div`
+  padding: 25px 20px;
+
+`
+
+const ImageWrapper = styled.div`
+  position: relative;
+
+  &:hover {
+    &::before {
+      content: "";
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      background: rgba(0, 0, 0, 0.3);
+      backdrop-filter: blur(4px);
+    }
+
+    ${Button} {
+      opacity: 1;
+    }
+  }
+
+  ${Button} {
+    opacity: 0;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    
+    &::before{
+      width: 100%;
+      height: 100%;
+    }
+
+  }
+
 
 `
 const Image = styled.img`
   width: 100%;
   height: 260px;
+  object-fit: cover;
 
 `
 
 const Title = styled.h3`
+  text-transform: capitalize;
 
 `
 const Text = styled.p`
+  margin: 14px 0 10px;
+
 
 `
-const Link = styled.a`
 
-`
 
