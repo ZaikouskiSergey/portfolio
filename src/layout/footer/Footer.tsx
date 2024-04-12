7a1aaa14970b3ab746319629af479c45af215e69
Sergey <Zaikouski.sergey@gmail.com>
2024-04-12 15:09:37 +0300

    feat: add update footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "components/icon/Icon";
 import {FlexWrapper} from "components/FlexWrapper";
+import {theme} from "styles/Theme";
 
 export const Footer = () => {
     return (
         <StyledFooter>
             <FlexWrapper direction={"column"} align={"center"}>
-                <Name>Siarhei</Name>
+                <Name>Siarhei Zaikouski</Name>
                 <SocialList>
                     <SocialItem>
                         <SocialLink>
@@ -28,20 +29,45 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  background-color: mediumpurple;
-  min-height: 20vh;
+  background-color: ${theme.colors.primiryBg};
+  padding: 40px 0;
 `
 const Name = styled.h5`
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 700;
+  font-size: 22px;
+  letter-spacing: 3px;
+  
 `
 const SocialList = styled.ul`
   display: flex;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0;
   
 `
 const SocialItem = styled.li`
     
+    
 `
 const SocialLink = styled.a`
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 35px;
+  height: 35px;
+  
+  color: ${theme.colors.accent};
+  &:hover{
+    color: ${theme.colors.primiryBg};
+    transform: translateY(-2px);
+  }
 `
 const Copyright = styled.small`
+  
+  font-weight: 400;
+  font-size: 12px;
+  text-align: center;
+  opacity: 0.5;
 `
