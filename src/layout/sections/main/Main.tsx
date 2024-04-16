7e3799374b6c6d25dd7d6c47ba3670e9cf061daf
Sergey <Zaikouski.sergey@gmail.com>
2024-04-16 12:39:44 +0300

    feat: change main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,37 +4,38 @@ import avatar from "../../../assets/images/my_new-avatar-webp.webp"
 import {FlexWrapper} from "components/FlexWrapper";
 import {Container} from "components/Container";
 import {theme} from "styles/Theme";
+import {font} from "styles/Common";
 
 export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={'wrap'}>
                     <div>
                         <SmallText>Hi There,</SmallText>
                         <MainName>I am <span>Siarhei Zaikouski</span></MainName>
                         <MainTitle>A Front-end Developer.</MainTitle>
                     </div>
+
                     <PhotoWrapper>
                         <Photo src={avatar} alt="avatar"/>
                     </PhotoWrapper>
-
                 </FlexWrapper>
             </Container>
         </StyledMain>
     );
 };
-const StyledMain = styled.div`
-  background-color: #1F1F20;
+const StyledMain = styled.section`  
   min-height: 100vh;
-  display: flex;
+  display: flex; 
 `
 
 const PhotoWrapper = styled.div`
   position: relative;
   z-index: 0;
+  margin-top: 65px;  
 
-  &::before{
+  &::before {
     z-index: -1;
     content: " ";
     width: 360px;
@@ -43,29 +44,41 @@ const PhotoWrapper = styled.div`
 
     position: absolute;
     top: -24px;
-    left: 24px;   
+    left: 24px;
+    @media ${theme.media.mobile} {
+      width: 314px;
+      height: 414px;
+      top: -17px;
+      left: 20px;
+
+    }
   }
+
 `
 const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  margin-right: 20px;
+  margin-left: 5px;
+
+  @media ${theme.media.mobile} {
+    width: 310px;
+    height: 380px;
+  }
 `
 const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400
-
+  ${font({weight: 400, fMax: 27, fMin: 20})};
 `
 const MainName = styled.h2`
-  font-family: "Josefin Sans", sans-serif;
-  font-size: 50px;
+  ${font({family: "'Josefin Sans', sans-serif", weight: 700, fMax: 50, fMin: 36})};
   letter-spacing: 0.05em;
-  font-weight: 700;
   margin: 10px 0;
-
+  
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
 
     &::before {
       content: "";
@@ -77,6 +90,10 @@ const MainName = styled.h2`
       bottom: 0;
       z-index: -1;
     }
+  }
+  
+  @media ${theme.media.mobile}{
+    margin: 15px 0 22px ;
   }
 
 
