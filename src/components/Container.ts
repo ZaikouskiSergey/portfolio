77e3dc29ca452ce7a47a9470bc52777048372423
Sergey <Zaikouski.sergey@gmail.com>
2024-04-22 18:21:51 +0300

    feat: refactor components

--- src/components/Container.ts
+++ src/components/Container.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-
 export const Container =styled.div`
   max-width: 1170px;
   width: 100%;
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import {Icon} from "components/icon/Icon";
 
-
 export const Logo = () => {
     return (
         <a href='#'>
-           <Icon iconId={"icon-logo"} width={'30'}/>
+            <Icon iconId={"icon-logo"} width={'30'}/>
         </a>
     );
 };
