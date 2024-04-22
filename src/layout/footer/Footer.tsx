d3fb7c445eab1aee00e088de30497812f456915f
Sergey <Zaikouski.sergey@gmail.com>
2024-04-22 18:12:14 +0300

    feat: refactor footer

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,79 +1,30 @@
 import React from 'react';
-import styled from "styled-components";
 import {Icon} from "components/icon/Icon";
 import {FlexWrapper} from "components/FlexWrapper";
-import {theme} from "styles/Theme";
-import {font} from "styles/Common";
+import {S} from "./Footer_Styles";
+
+const socialItemData = [
+    {iconId: "icon-react"},
+    {iconId: "icon-react"},
+    {iconId: "icon-react"}
+]
 
 export const Footer = () => {
     return (
-        <StyledFooter>
+        <S.Footer>
             <FlexWrapper direction={"column"} align={"center"}>
-                <Name>Siarhei Zaikouski</Name>
-                <SocialList>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21"} width={"21"} iconId={"icon-react"}/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21"} width={"21"} iconId={"icon-react"}/>
-                        </SocialLink>
-                    </SocialItem>
-                    <SocialItem>
-                        <SocialLink>
-                            <Icon height={"21"} width={"21"} iconId={"icon-react"}/>
-                        </SocialLink>
-                    </SocialItem>
-                </SocialList>
-                <Copyright>© 2024 Siarhei Zaikouski, All Rights Reserved.</Copyright>
+                <S.Name>Siarhei Zaikouski</S.Name>
+                <S.SocialList>
+                    {socialItemData.map((icon, index) =>
+                        <S.SocialItem key={index}>
+                            <S.SocialLink>
+                                <Icon height={"21"} width={"21"} viewBox={"0 0 120 120"} iconId={icon.iconId}/>
+                            </S.SocialLink>
+                        </S.SocialItem>)}
+                </S.SocialList>
+                <S.Copyright>© 2024 Siarhei Zaikouski, All Rights Reserved.</S.Copyright>
             </FlexWrapper>
-
-        </StyledFooter>
+        </S.Footer>
     );
 };
 
-const StyledFooter = styled.footer`
-  background-color: ${theme.colors.primiryBg};
-  padding: 40px 0;
-`
-const Name = styled.h5`
-  ${font({family: "'Josefin Sans', sans-serif", weight: 700, fMax: 22, fMin: 16})};  
-  letter-spacing: 3px;
-  
-  
-`
-const SocialList = styled.ul`
-  display: flex;
-  gap: 20px;
-  margin: 30px 0;
-  
-`
-const SocialItem = styled.li`
-    
-    
-`
-const SocialLink = styled.a`
-  background-color: rgba(255, 255, 255, 0.1);
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 35px;
-  height: 35px;
-  
-  color: ${theme.colors.accent};
-  &:hover{
-    color: ${theme.colors.primiryBg};
-    transform: translateY(-3px);
-    background-color: ${theme.colors.accent};
-  }
-`
-const Copyright = styled.small`
-  
-  font-weight: 400;
-  font-size: 12px;
-  text-align: center;
-  opacity: 0.5;
-`
---
+++ src/layout/footer/Footer_Styles.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import {theme} from "styles/Theme";
+import {font} from "styles/Common";
+
+const Footer = styled.footer`
+  background-color: ${
+          theme.colors.primiryBg
+  };
+  padding: 40px 0;
+`
+const Name = styled.h5`
+  ${
+          font({family: "'Josefin Sans', sans-serif", weight: 700, fMax: 22, fMin: 16})
+  };
+  letter-spacing: 3px;
+
+
+`
+const SocialList = styled.ul`
+  display: flex;
+  gap: 20px;
+  margin: 30px 0;
+`
+const SocialItem = styled.li`
+`
+const SocialLink = styled.a`
+  background-color: rgba(255, 255, 255, 0.1);
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 35px;
+  height: 35px;
+
+  color: ${
+          theme.colors.accent
+  };
+
+  &:hover {
+    color: ${theme.colors.primiryBg};
+    transform: translateY(-3px);
+    background-color: ${theme.colors.accent};
+  }
+`
+const Copyright = styled.small`
+  font-weight: 400;
+  font-size: 12px;
+  text-align: center;
+  opacity: 0.5;
+`
+export const S = {
+    Footer,
+    Name,
+    SocialList,
+    SocialLink,
+    SocialItem,
+    Copyright
+}
