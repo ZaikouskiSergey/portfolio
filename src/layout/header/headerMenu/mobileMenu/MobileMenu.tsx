2797dc2256b320917e743f287787f274307dd2ac
Sergey <Zaikouski.sergey@gmail.com>
2024-04-23 13:27:53 +0300

    feat: add open-close to menu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Menu} from "layout/header/headerMenu/menu/Menu";
 import {S} from 'layout/header/headerMenu/HeaderMenu_Styles';
 
@@ -6,12 +6,18 @@ type MenuItemsProps = {
     items: string[]
 }
 export const MobileMenu: React.FC<MenuItemsProps> = ({items}: MenuItemsProps) => {
+    const [menuIsOpen, setMenuIsOpen] = useState(false)
+
+    const onBurgerClick =()=>{
+        setMenuIsOpen(!menuIsOpen)
+    }
+
     return (
         <S.MobileMenu>
-            <S.BurgerButton isOpen={false}>
+            <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerClick}>
                 <span></span>
             </S.BurgerButton>
-            <S.MobileMenuPopup isOpen={false}>
+            <S.MobileMenuPopup isOpen={menuIsOpen} onClick={()=> setMenuIsOpen(false)}>
                 <Menu items={items}/>
             </S.MobileMenuPopup>
         </S.MobileMenu>
